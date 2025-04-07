a0c371384a6b1f9a1bd3b29bba5de373129d20d7
Michelle <mpuschkarow@web.de>
2025-04-07 18:25:28 +0200

    url for gif-img added & color design changed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let limit = 40;
 let offet = 0;
+let imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"
 
 function init() {
   fetchPokemonUrls();
@@ -19,19 +20,22 @@ async function getEachPokemonUrl(array) {
     let response = await fetch(entry.url);
     let pokemonToJson = await response.json();
     renderPokemons(pokemonToJson, i);
+    console.log(entry.url);
   }
 }
 
 function renderPokemons(pokemon, i) {
   let contentContainer = document.getElementById("content");
   contentContainer.innerHTML += `
-        <div id="pokemon_card" class="pokemon_card" onclick="openDialogOverlay(${i + 1})">
-            <div>
-                <p> #${i + 1}</p>
+        <div id="pokemon_card${i}" class="pokemon_card" onclick="openDialogOverlay(${i})">
+            <div class="pokemon_title">
+                <p> #${(i + 1)}</p>
                 <p> ${pokemon.species.name}</p>
             </div>
-           <img src="#">
-           <p> ${pokemon.weight}</p>
+            <div class="bg_for_img">
+             <img class="pokemon_img" src="${imgUrl+(i + 1)}.gif">
+            </div>
+           <p>weight: ${pokemon.weight} kg</p>
         </div>
        `;
 }
@@ -45,4 +49,23 @@ function loadMoreData() {
     offet += 40;
     fetchPokemonUrls();
   }
+}
+
+function openDialogOverlay(index, pokemon){
+console.log(index)
+    // let dialog = document.getElementById("dialog_overlay");
+    // dialog.innerHTML += `
+    //       <div id="pokemon_dialog${index}" class="pokemon_card">
+    //           <div>
+    //               <p> #${index + 1}</p>
+    //               <p> ${pokemon.species.name}</p>
+    //           </div>
+    //          <img src="#">
+    //          <p> ${pokemon.weight}</p>
+    //          <div>
+    //              <button>left</button>
+    //              <button>right</button>
+    //          </div> 
+    //       </div>
+    //      `;
 }
---
+++ scripts/template.js
@@ -0,0 +1,5 @@
+//gif-url https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/
+
+//png-shiny-url https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/
+//png https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/
+//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/shiny/
