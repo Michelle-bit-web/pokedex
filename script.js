dbb7d961a1a1630e50c00245d2005bac9f57b475
Michelle <mpuschkarow@web.de>
2025-04-07 13:48:49 +0200

    function to render a pokemon selection

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ function init(){
 }
 
 async function fetchPokemonData(){
-let response = await fetch("https://pokeapi.co/api/v2/");
+const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=40&offset=0");
 let responseToJson = await response.json();
-console.log(responseToJson);
+console.log(response);
+console.log(responseToJson); //über limit Anzahl änderbar und mit offset Startpunkt der Anzahl
+renderRequest(responseToJson);
+}
+
+function renderRequest(array){
+    let contentContainer = document.getElementById('content');
+    let allPokemon = array.results;
+    console.log(allPokemon)
+    contentContainer.innerHTML = "";
+
+    allPokemon.forEach(singlePokemon =>{
+        contentContainer.innerHTML += `
+        <div id="pokemon_card" class="pokemon_card">
+          <p> ${singlePokemon.name}</p>
+        </div>
+       `} )
 }
