758295d3256154526d88be46d3769220082fef4f
Michelle <mpuschkarow@web.de>
2025-04-07 19:41:02 +0200

    dialog overlay placed with close-button and disabling scrolling

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let limit = 40;
-let offet = 0;
-let imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"
+let offset = 0;
+let pokemonData = [];
+let imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/";
+let openDialog = false;
 
 function init() {
   fetchPokemonUrls();
 }
 
 async function fetchPokemonUrls() {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offet}`);
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
   let responseToJson = await response.json();
   getEachPokemonUrl(responseToJson);
 }
@@ -15,7 +17,7 @@ async function fetchPokemonUrls() {
 //Eine allgemeine URL bauen: "https://pokeapi.co/api/v2/${feature}/${i}/" wo feature und id austauschbar sind
 
 async function getEachPokemonUrl(array) {
-  for (let i = 0; i < array.results.length; i++) {
+  for (let i = offset; i < array.results.length; i++) {
     const entry = array.results[i];
     let response = await fetch(entry.url);
     let pokemonToJson = await response.json();
@@ -25,15 +27,16 @@ async function getEachPokemonUrl(array) {
 }
 
 function renderPokemons(pokemon, i) {
+  pokemonData[i] = pokemon;
   let contentContainer = document.getElementById("content");
   contentContainer.innerHTML += `
         <div id="pokemon_card${i}" class="pokemon_card" onclick="openDialogOverlay(${i})">
             <div class="pokemon_title">
-                <p> #${(i + 1)}</p>
+                <p> #${i + 1}</p>
                 <p> ${pokemon.species.name}</p>
             </div>
             <div class="bg_for_img">
-             <img class="pokemon_img" src="${imgUrl+(i + 1)}.gif">
+             <img class="pokemon_img" src="${imgUrl + (i + 1)}.gif">
             </div>
            <p>weight: ${pokemon.weight} kg</p>
         </div>
@@ -41,31 +44,54 @@ function renderPokemons(pokemon, i) {
 }
 
 function loadMoreData() {
-  if (limit >= 120) {
+  if (limit >= 350) {
     document.getElementById("load_data_btn").disabled = true;
   } else {
     console.log("is loading");
     limit += 40;
-    offet += 40;
+    offset += 40;
     fetchPokemonUrls();
   }
 }
 
-function openDialogOverlay(index, pokemon){
-console.log(index)
-    // let dialog = document.getElementById("dialog_overlay");
-    // dialog.innerHTML += `
-    //       <div id="pokemon_dialog${index}" class="pokemon_card">
-    //           <div>
-    //               <p> #${index + 1}</p>
-    //               <p> ${pokemon.species.name}</p>
-    //           </div>
-    //          <img src="#">
-    //          <p> ${pokemon.weight}</p>
-    //          <div>
-    //              <button>left</button>
-    //              <button>right</button>
-    //          </div> 
-    //       </div>
-    //      `;
+function openDialogOverlay(index) {
+  let dialog = document.getElementById("dialog_overlay");
+  let pokemon = pokemonData[index];
+
+  dialog.classList.remove("d_none");
+  dialog.innerHTML = `
+      <div id="pokemon_dialog${index}" class="pokemon_card">
+          <div class="dialog_top_content">
+                <div>
+                    <p>#${index + 1}</p>
+                    <p>${pokemon.species.name}</p>
+              
+                </div>
+                <button onclick="closeDialog()" class="close_btn">Close</button>
+          </div>
+          <div class="bg_for_img">
+              <img class="pokemon_img" src="${imgUrl + (index + 1)}.gif">
+          </div>
+          <p>weight: ${pokemon.weight} kg</p>
+          <div>
+              <button onclick="navigateDialog(${index - 1})">left</button>
+              <button onclick="navigateDialog(${index + 1})">right</button>
+          </div>
+      </div>
+    `;
+  unableScrolling();
+}
+
+function unableScrolling() {
+  document.body.style.overflow = "hidden";
+}
+
+function enableScrolling() {
+  document.body.style.overflow = "";
+}
+
+function closeDialog() {
+  let dialog = document.getElementById("dialog_overlay");
+  dialog.classList.add("d_none");
+  enableScrolling();
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,5 +1,4 @@
 //gif-url https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/
-
 //png-shiny-url https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/
 //png https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/
 //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/shiny/
