c32113c8c65f465dee7607d4fec97c966c7ae0be
Michelle <mpuschkarow@web.de>
2025-04-07 16:13:24 +0200

    onclick function for loading more data

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-function init(){
-    fetchPokemonData();
+let limit = 40;
+let offet = 0;
+
+function init() {
+  fetchPokemonUrls();
 }
 
-async function fetchPokemonData(){
-const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=40&offset=0");
-let responseToJson = await response.json();
-console.log(response);
-console.log(responseToJson); //über limit Anzahl änderbar und mit offset Startpunkt der Anzahl
-renderRequest(responseToJson);
+async function fetchPokemonUrls() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offet}`);
+  let responseToJson = await response.json();
+  getEachPokemonUrl(responseToJson);
 }
 
-function renderRequest(array){
-    let contentContainer = document.getElementById('content');
-    let allPokemon = array.results;
-    console.log(allPokemon)
-    contentContainer.innerHTML = "";
+//Eine allgemeine URL bauen: "https://pokeapi.co/api/v2/${feature}/${i}/" wo feature und id austauschbar sind
 
-    allPokemon.forEach(singlePokemon =>{
-        contentContainer.innerHTML += `
-        <div id="pokemon_card" class="pokemon_card">
-          <p> ${singlePokemon.name}</p>
+async function getEachPokemonUrl(array) {
+  for (let i = 0; i < array.results.length; i++) {
+    const entry = array.results[i];
+    let response = await fetch(entry.url);
+    let pokemonToJson = await response.json();
+    renderPokemons(pokemonToJson, i);
+  }
+}
+
+function renderPokemons(pokemon, i) {
+  let contentContainer = document.getElementById("content");
+  contentContainer.innerHTML += `
+        <div id="pokemon_card" class="pokemon_card" onclick="openDialogOverlay(${i + 1})">
+            <div>
+                <p> #${i + 1}</p>
+                <p> ${pokemon.species.name}</p>
+            </div>
+           <img src="#">
+           <p> ${pokemon.weight}</p>
         </div>
-       `} )
+       `;
+}
+
+function loadMoreData() {
+  if (limit >= 120) {
+    document.getElementById("load_data_btn").disabled = true;
+  } else {
+    console.log("is loading");
+    limit += 40;
+    offet += 40;
+    fetchPokemonUrls();
+  }
 }
