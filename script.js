1654664cee605da30e5febabe8475f56b94bba1a
Michelle <mpuschkarow@web.de>
2025-04-08 08:48:17 +0200

    categories for dialog informations added

--- script.js
+++ script.js
@@ -9,18 +9,23 @@ function init() {
   fetchPokemonUrls();
 }
 
+//hier vllt eine Hilfsvariable bauen, die immer Urls fetched & mit Parametern oder sonst default parametern
+//Beispiel let pokemonForm = "pokemon-form"  
+//Dann Funktionsaufruf mit übergabe von pokemonForm
+//Dann irgendwie iteration um übergabe von i zu kriegen
 async function fetchPokemonUrls() {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
   let responseToJson = await response.json();
   getEachPokemonUrl(responseToJson);
 }
 
+//hier URL für name, types,png https://pokeapi.co/api/v2/pokemon-form/1/
 //Eine allgemeine URL bauen: "https://pokeapi.co/api/v2/${feature}/${i}/" wo feature und id austauschbar sind
 
 async function getEachPokemonUrl(array) {
   for (let i = offset; i < array.results.length; i++) {
     const entry = array.results[i];
-    let response = await fetch(entry.url);
+    let response = await fetch(entry.url);//hier vielleicht eine generelle Übergabe von Urls, die gefetched werden und an bestimmte Fkt übergeben werden
     let pokemonToJson = await response.json();
     renderPokemons(pokemonToJson, i);
     console.log(entry.url);
@@ -48,10 +53,12 @@ function loadMoreData() {
   if (limit >= 350) {
     document.getElementById("load_data_btn").disabled = true;
   } else {
+    document.getElementById("load_data_btn").disabled = true;
     console.log("is loading");
     limit += 40;
     offset += 40;
     fetchPokemonUrls();
+    document.getElementById("load_data_btn").disabled = false;
   }
 }
 
@@ -61,7 +68,7 @@ function openDialogOverlay(index) {
 
   dialog.classList.remove("d_none");
   dialog.innerHTML = `
-      <div id="pokemon_dialog${index}" class="pokemon_card">
+      <div id="pokemon_dialog${index}" class="pokemon_card_dialog">
           <div class="dialog_top_content">
                 <div>
                     <p>#${index + 1}</p>
@@ -70,10 +77,21 @@ function openDialogOverlay(index) {
                 </div>
                 <button onclick="closeDialog()" class="close_btn">Close</button>
           </div>
-          <div class="bg_for_img">
-              <img class="pokemon_img" src="${imgUrl + (index + 1)}.gif">
+          <div class="bg_for_img_dialog">
+              <img class="pokemon_img_dialog" src="${imgUrl + (index + 1)}.gif">
           </div>
-          <p>weight: ${pokemon.weight} kg</p>
+          <div class="categories_dialog">
+              <div class="category_titles_dialog">
+              <h3 class="onclick"> category 1</h3>
+              <h3> category 2</h3>
+              <h3> category 3</h3>
+              <h3> category 4</h3>
+              </div>
+              <p>weight: ${pokemon.weight} kg</p>
+              <p>weight: ${pokemon.weight} kg</p>
+              <p>weight: ${pokemon.weight} kg</p>
+          </div>
+          
           <div>
               <button onclick="navigateDialog(${index}, -1)">left</button>
               <button onclick="navigateDialog(${index}, 1)">right</button>
